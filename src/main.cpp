d29499348975eec3809efc20eaf6b08a2f6a4df0
Ira Woodring <irawoodring@gmail.com>
2021-02-18 17:58:42 -0500

    Add comments for usage instructions.

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,19 @@ int main(int argc, char** argv){
 		SDL_Log("%d = %s", i, argv[i]);
 	}
 
+	// Create a scene
 	Scene one;
+	// Create an engine.  Must happen early, creates the renderer.
 	Engine engine(1024, 768);
+
+	// Make a banana and add to scene. Should update and draw.
 	Banana* b = new Banana();
 	one.addUpdateable(b);
 	one.addDrawable(b);
+
+	// Set the scene in the engine
 	engine.setScene(&one);
+	
+	// Get the engine running.
 	engine.run();
 }
